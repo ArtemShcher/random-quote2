cdcf449253673367ff6b67f068ace3747f249a16
Artem Shcherbakov <shcherbakovartemy@gmail.comv>
2025-02-14 09:10:04 +0200

    Spliting app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,47 @@
+const quotes = [
+  {
+    quote: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life.",
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      'Life is like riding a bicycle. To keep your balance you must keep moving.',
+    author: 'Albert Einstein',
+  },
+  {
+    quote: 'Be the change that you wish to see in the world.',
+    author: 'Mahatma Gandhi',
+  },
+  {
+    quote: 'The secret of getting ahead is getting started.',
+    author: 'Mark Twain',
+  },
+  {
+    quote:
+      'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+    author: 'Winston Churchill',
+  },
+  {
+    quote: "It always seems impossible until it's done.",
+    author: 'Nelson Mandela',
+  },
+  {
+    quote: 'Be yourself; everyone else is already taken.',
+    author: 'Oscar Wilde',
+  },
+  {
+    quote: 'An investment in knowledge always pays the best interest.',
+    author: 'Benjamin Franklin',
+  },
+]
+
+export default quotes
--- script.js
+++ script.js
@@ -1,48 +1,4 @@
-const quotes = [
-  {
-    quote: 'The only way to do great work is to love what you do.',
-    author: 'Steve Jobs',
-  },
-  {
-    quote: 'Innovation distinguishes between a leader and a follower.',
-    author: 'Steve Jobs',
-  },
-  {
-    quote:
-      "Your time is limited, so don't waste it living someone else's life.",
-    author: 'Steve Jobs',
-  },
-  {
-    quote:
-      'Life is like riding a bicycle. To keep your balance you must keep moving.',
-    author: 'Albert Einstein',
-  },
-  {
-    quote: 'Be the change that you wish to see in the world.',
-    author: 'Mahatma Gandhi',
-  },
-  {
-    quote: 'The secret of getting ahead is getting started.',
-    author: 'Mark Twain',
-  },
-  {
-    quote:
-      'Success is not final, failure is not fatal: it is the courage to continue that counts.',
-    author: 'Winston Churchill',
-  },
-  {
-    quote: "It always seems impossible until it's done.",
-    author: 'Nelson Mandela',
-  },
-  {
-    quote: 'Be yourself; everyone else is already taken.',
-    author: 'Oscar Wilde',
-  },
-  {
-    quote: 'An investment in knowledge always pays the best interest.',
-    author: 'Benjamin Franklin',
-  },
-]
+import allQuotes from './quotes.js'
 
 const quoteElement = document.getElementById('quote')
 const quoteAuthorElement = document.getElementById('quote-author')
@@ -50,8 +6,8 @@ const generateBtn = document.getElementById('generate-btn')
 
 function generateRandomQuote() {
   console.log('This function was called')
-  const randomIndex = Math.floor(Math.random() * quotes.length)
-  const randomQuote = quotes[randomIndex]
+  const randomIndex = Math.floor(Math.random() * allQuotes.length)
+  const randomQuote = allQuotes[randomIndex]
   const { quote, author } = randomQuote
   quoteElement.textContent = quote
   quoteAuthorElement.textContent = author
