06dd9b8ec337baa4db6df8a38ea80044fb437e72
Artem Shcherbakov <shcherbakovartemy@gmail.comv>
2025-02-19 09:17:12 +0200

    Show favorite quotes below current quote

--- quotes.js
+++ quotes.js
@@ -2,45 +2,55 @@ const quotes = [
   {
     quote: 'The only way to do great work is to love what you do.',
     author: 'Steve Jobs',
+    isFaavorite: false,
   },
   {
     quote: 'Innovation distinguishes between a leader and a follower.',
     author: 'Steve Jobs',
+    isFaavorite: false,
   },
   {
     quote:
       "Your time is limited, so don't waste it living someone else's life.",
     author: 'Steve Jobs',
+    isFaavorite: false,
   },
   {
     quote:
       'Life is like riding a bicycle. To keep your balance you must keep moving.',
     author: 'Albert Einstein',
+    isFaavorite: false,
   },
   {
     quote: 'Be the change that you wish to see in the world.',
     author: 'Mahatma Gandhi',
+    isFaavorite: false,
   },
   {
     quote: 'The secret of getting ahead is getting started.',
     author: 'Mark Twain',
+    isFaavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: it is the courage to continue that counts.',
     author: 'Winston Churchill',
+    isFaavorite: false,
   },
   {
     quote: "It always seems impossible until it's done.",
     author: 'Nelson Mandela',
+    isFaavorite: false,
   },
   {
     quote: 'Be yourself; everyone else is already taken.',
     author: 'Oscar Wilde',
+    isFaavorite: false,
   },
   {
     quote: 'An investment in knowledge always pays the best interest.',
     author: 'Benjamin Franklin',
+    isFavorite: false,
   },
 ]
 
